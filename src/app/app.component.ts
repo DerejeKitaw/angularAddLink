c1213192e2c6dce0c2e0a6df9a8479ec0f36ee11
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-06-10 11:47:02 -0400

    total vote updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
 import { Article } from './article.model';
 
 
@@ -7,9 +7,14 @@ import { Article } from './article.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   article: Article;
   articles: Article[];
+  totalPoints;
+  ngOnInit(): void {
+    this.totalPoints = 0;
+    this.calcTotalPoints();
+  }
   constructor() {
     this.articles = [
       new Article('Angular 2', 'http://angular.io', 3),
@@ -17,6 +22,7 @@ export class AppComponent {
       new Article('Angular Homepage', 'http://angular.io', 1)
     ];
   }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     this.articles.push(new Article(title.value, link.value, 0));
@@ -31,19 +37,28 @@ export class AppComponent {
   voteUp(titleKey): boolean {
     this.article = this.selectArticle(titleKey, this.articles);
     this.article.voteUp();
+    this.calcTotalPoints();
     return false;
   }
 
   voteDown(titleKey): boolean {
     this.article = this.selectArticle(titleKey, this.articles);
     this.article.voteDown();
+    this.calcTotalPoints();
     return false;
   }
   selectArticle(titleKey, myArray) {
     for (let i = 0; i < myArray.length; i++) {
+      this.totalPoints = this.totalPoints + Number(myArray[i].votes) * 1;
         if (myArray[i].title === titleKey) {
             return myArray[i];
         }
+    }
+}
+  calcTotalPoints() {
+    this.totalPoints = 0;
+    for (let i = 0; i < this.articles.length; i++) {
+      this.totalPoints = this.totalPoints + this.articles[i].votes * 1;
     }
 }
 }
